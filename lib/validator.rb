722a893fc23ce37001798a1eb056d83345df920b
BTosty <tharvijs63@gmail.com>
2022-11-16 00:36:55 +0200

    Valid + comments
    Added comments. No changes in code since didn't seem really necessary.

--- lib/validator.rb
+++ lib/validator.rb
@@ -8,7 +8,9 @@ class Validator
   end
 
   def validate
+    #Strips given sudoku and leaves only the numbers
     string = @puzzle_string.delete("|").delete("-").delete("+").gsub(/\s+/, "")
+    #Validation process
     if checkrow(string)
       return "Sudoku is invalid." 
     elsif checkcollumn(string)
@@ -27,6 +29,7 @@ class Validator
   def checkrow(string)
     for i in 0..8 do
       testi = " "
+      #Creates string of every row and checks if they are valid by seeing if they have repeated characters
       testi = string[i*9..i*9+8]
       if testi.delete("0").chars.count{ |char| testi.count(char) > 1 } > 0
         return true
@@ -39,6 +42,7 @@ class Validator
   def checkcollumn(string)
     for i in 0..8 do
       testi = " "
+      #Creates string of every collumn and checks if they are valid by seeing if they have repeated characters
       for j in 0..8 do
         testi = testi + string[i+j*9]
       end
@@ -53,6 +57,7 @@ class Validator
   def checkbox(string)
     for i in 0..2 do
       testi = " "
+      #Creates string of every box and checks if they are valid by seeing if they have repeated characters
       for j in 0..2 do
         testi = string[j+(j*2)+(i*27)..j+(j*2)+(i*27)+2] + string[j+(j*2)+i*27+9..j+(j*2)+i*27+9+2] + string[j+(j*2)+i*27+9*2..j+(j*2)+i*27+9*2+2]
         if testi.delete("0").chars.count{ |char| testi.count(char) > 1 } > 0
